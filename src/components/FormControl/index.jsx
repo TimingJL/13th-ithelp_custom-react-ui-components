1731bcb5a9c35457c4804fe0fb6cca48a8000556
taiming <taiming@bridgewell.com>
2021-09-16 01:08:58 +0800

    Add form control

---
+++ src/components/FormControl/index.jsx
@@ -0,0 +1,199 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const topCommonStyle = css`
+  flex-direction: column;
+  & > *:not(:first-child) {
+    margin-top: 8px;
+  }
+`;
+
+const bottomCommonStyle = css`
+  flex-direction: column-reverse;
+  & > *:not(:first-child) {
+    margin-bottom: 8px;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  font-size: 14px;
+  margin-top: 4px !important;
+  color: ${(props) => props.theme.color.error};
+`;
+
+const LabelWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MaxLength = styled.div`
+  font-size: 14px;
+  color: ${(props) => props.theme.color.primary};
+  display: flex;
+  align-items: flex-end;
+`;
+
+const RequiredSign = styled.span`
+  color: ${(props) => props.theme.color.error};
+`;
+
+const topLeftStyle = css`
+  ${topCommonStyle}
+`;
+const topStyle = css`
+  align-items: center;
+  ${topCommonStyle}
+`;
+const topRightStyle = css`
+  align-items: flex-end;
+  ${topCommonStyle}
+`;
+const bottomLeftStyle = css`
+  ${bottomCommonStyle}
+`;
+const bottomStyle = css`
+  align-items: center;
+  ${bottomCommonStyle}
+`;
+const bottomRightStyle = css`
+  align-items: flex-end;
+  ${bottomCommonStyle}
+`;
+
+const leftStyle = css`
+  align-items: center;
+  & > *:not(:first-child) {
+    margin-left: 8px;
+  }
+`;
+
+const rightStyle = css`
+  align-items: center;
+  flex-direction: row-reverse;
+  & > *:not(:first-child) {
+    margin-right: 8px;
+  }
+`;
+
+const placementStyleMap = {
+  'top-left': topLeftStyle,
+  top: topStyle,
+  'top-right': topRightStyle,
+  left: leftStyle,
+  right: rightStyle,
+  'bottom-left': bottomLeftStyle,
+  bottom: bottomStyle,
+  'bottom-right': bottomRightStyle,
+};
+
+const StyledFormControl = styled.div`
+  display: inline-flex;
+  ${(props) => {
+    if (props.$placement) {
+      return placementStyleMap[props.$placement];
+    }
+    return null;
+  }}
+`;
+
+const FormControl = ({
+  label,
+  children,
+  maxLength,
+  placement,
+  isError,
+  errorMessage,
+  isRequired,
+  onChange,
+  className,
+  ...props
+}) => {
+  const [childrenValue, setChildrenValue] = useState('');
+  const isSwitchComponent = children.type.name === 'Switch';
+
+  const handleOnChange = (event) => {
+    const targetValue = event?.target?.value;
+    if (maxLength && targetValue.length > maxLength) return;
+
+    setChildrenValue(targetValue);
+    if (typeof onChange === 'function') {
+      onChange(event);
+    }
+  };
+
+  return (
+    <StyledFormControl className={className} $placement={placement} {...props}>
+      <LabelWrapper className="form-control__label-wrapper">
+        <div className="form-control__label">
+          {label}
+          {isRequired && <RequiredSign>*</RequiredSign>}
+        </div>
+        {maxLength && <MaxLength>{`${childrenValue?.length} / ${maxLength}`}</MaxLength>}
+      </LabelWrapper>
+      {
+        isSwitchComponent ? children : (
+          React.cloneElement(children, {
+            isError,
+            value: childrenValue,
+            onChange: handleOnChange,
+          })
+        )
+      }
+      {(isError && errorMessage) && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </StyledFormControl>
+  );
+};
+
+FormControl.propTypes = {
+  /**
+   * 客製化 class 樣式
+   */
+  className: PropTypes.string,
+  /**
+   * 標示為必填
+   */
+  isRequired: PropTypes.bool,
+  /**
+   * 顯示錯誤樣式
+   */
+  isError: PropTypes.bool,
+  /**
+   * 顯示錯誤訊息
+   */
+  errorMessage: PropTypes.string,
+  /**
+   * 限制最大輸入長度
+   */
+  maxLength: PropTypes.number,
+  /**
+   * 標題位置
+   */
+  placement: PropTypes.oneOf(['top-left', 'top', 'top-right', 'left', 'right', 'bottom-left', 'bottom', 'bottom-right']),
+  /**
+   * 標題內容
+   */
+  label: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  /**
+   *  狀態改變的 callback function
+   */
+  onChange: PropTypes.func,
+  /**
+   * 要管理的 form 內容
+   */
+  children: PropTypes.element,
+};
+
+FormControl.defaultProps = {
+  className: '',
+  isRequired: false,
+  isError: false,
+  errorMessage: null,
+  placement: 'top-left',
+  maxLength: null,
+  label: '',
+  onChange: () => {},
+  children: null,
+};
+
+export default FormControl;
---
+++ src/stories/FormControl.stories.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import FormControl from '../components/FormControl';
+import TextField from '../components/TextField';
+import Radio from '../components/Radio';
+import Switch from '../components/Switch';
+
+export default {
+  title: '數據輸入元件/FormControl',
+  component: FormControl,
+  argTypes: {
+    themeColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <FormControl {...args} />;
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  label: '請輸入資料',
+  children: <TextField placeholder="請輸入" />,
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  label: '請輸入資料',
+  isRequired: true,
+  children: <TextField placeholder="請輸入" />,
+};
+
+export const LimitMaxLength = Template.bind({});
+LimitMaxLength.args = {
+  label: '請輸入資料',
+  maxLength: 12,
+  children: <TextField placeholder="請輸入" />,
+};
+
+export const ErrorMessage = Template.bind({});
+ErrorMessage.args = {
+  isError: true,
+  errorMessage: '請檢查輸入是否錯誤',
+  label: '請輸入資料',
+  children: <TextField placeholder="請輸入" />,
+};
+
+const Row = styled.div`
+  display: flex;
+  & > *:not(:first-child) {
+    margin-left: 40px;
+  }
+`;
+
+const Column = styled.div`
+  & > *:not(:first-child) {
+    margin-top: 40px;
+  }
+`;
+
+const TemplateWithPlacement = (args) => (
+  <Column>
+    <Row>
+      <FormControl {...args} placement="top-left" label="左上標題" />
+      <FormControl {...args} placement="top" label="正上標題" />
+      <FormControl {...args} placement="top-right" label="右上標題" />
+    </Row>
+    <Row><FormControl {...args} placement="left" label="左邊標題" /></Row>
+    <Row><FormControl {...args} placement="right" label="右邊標題" /></Row>
+    <Row>
+      <FormControl {...args} placement="bottom-left" label="左下標題" />
+      <FormControl {...args} placement="bottom" label="正下標題" />
+      <FormControl {...args} placement="bottom-right" label="右下標題" />
+    </Row>
+  </Column>
+);
+
+export const WithPlacement = TemplateWithPlacement.bind({});
+WithPlacement.args = {
+  children: <TextField placeholder="請輸入" />,
+};
+
+export const RadioWithPlacement = TemplateWithPlacement.bind({});
+RadioWithPlacement.args = {
+  children: <Radio />,
+};
+
+export const SwitchWithPlacement = TemplateWithPlacement.bind({});
+SwitchWithPlacement.args = {
+  children: <Switch />,
+};
