de0dc8c567fe8333a43582c8e789bc6ddadb568e
taiming <taiming@bridgewell.com>
2021-09-21 13:38:30 +0800

    Add reset-upload stories

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 /**
  * `Upload` 是一個上傳元件。幫助我們能夠發佈文字、圖片、影片、檔案到後端伺服器上。
 */
 const Upload = ({
+  resetKey,
   children,
-}) => (
-  <div>{children}</div>
-);
+  onChange,
+  ...props
+}) => {
+  const inputFileRef = useRef();
+
+  const handleOnClickUpload = () => {
+    inputFileRef.current.click();
+  };
+
+  const handleOnChange = (event) => {
+    if (typeof onChange === 'function') {
+      onChange(event?.target?.files);
+    }
+  };
+
+  useEffect(() => {
+    if (resetKey) {
+      inputFileRef.current.value = '';
+      handleOnChange();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resetKey]);
+
+  return (
+    <>
+      <input
+        ref={inputFileRef}
+        type="file"
+        style={{ display: 'none' }}
+        onChange={handleOnChange}
+        {...props}
+      />
+      {
+        React.cloneElement(children, {
+          onClick: handleOnClickUpload,
+        })
+      }
+    </>
+  );
+};
 
 Upload.propTypes = {
   /**
-   * 內容
+   * 重設鍵值
+   */
+  resetKey: PropTypes.number,
+  /**
+   * 限制檔案類型
+   */
+  accept: PropTypes.string,
+  /**
+   * 是否選取多個檔案
+   */
+  multiple: PropTypes.bool,
+  /**
+   * 選取上傳檔案時的 callback
+   */
+  onChange: PropTypes.func,
+  /**
+   * 內容，這邊指的是上傳按鈕外觀
    */
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
 };
 
 Upload.defaultProps = {
-
+  resetKey: 0,
+  accept: undefined,
+  multiple: false,
+  onChange: () => {},
 };
 
 export default Upload;
--- src/stories/Upload.stories.jsx
+++ src/stories/Upload.stories.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 import Upload from '../components/Upload';
 import Button from '../components/Button';
 
@@ -8,18 +11,62 @@ export default {
   component: Upload,
 };
 
-const TemplateDefault = (args) => (
-  <Upload
-    {...args}
-  >
-    <Button
-      variant="outlined"
-      startIcon={<CloudUploadIcon />}
-    >
-      上傳
-    </Button>
-  </Upload>
-);
+const SpaceBetween = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TemplateDefault = (args) => {
+  const [uploadFile, setUploadFile] = useState(null);
+  const [resetKey, setResetKey] = useState(0);
+
+  const handleResetUpload = () => {
+    setResetKey((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <SpaceBetween>
+        <Upload
+          {...args}
+          resetKey={resetKey}
+          onChange={(files) => setUploadFile(files ? files[0] : null)}
+        >
+          <Button
+            variant="outlined"
+            startIcon={<CloudUploadIcon />}
+          >
+            上傳
+          </Button>
+        </Upload>
+        <Button
+          variant="outlined"
+          startIcon={<RotateLeftIcon />}
+          onClick={handleResetUpload}
+        >
+          重設
+        </Button>
+      </SpaceBetween>
+      {uploadFile && (
+        <div style={{ marginTop: 20 }}>
+          <div>
+            檔案名稱：
+            <span>{uploadFile.name}</span>
+          </div>
+          <div>
+            檔案類型：
+            <span>{uploadFile.type}</span>
+          </div>
+          <div>
+            檔案大小：
+            <span>{`${uploadFile.size} Bytes`}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export const Default = TemplateDefault.bind({});
 Default.args = {};
