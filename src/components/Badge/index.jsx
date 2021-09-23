cef875da332f1c71f27a554adb5f89a6d4e38ef4
taiming <taiming@bridgewell.com>
2021-09-23 15:09:43 +0800

    Add Badge stories

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -1,5 +1,156 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Badge = () => <div>Badge</div>;
+import { useColor } from 'hooks/useColor';
+
+const topLeftStyle = css`
+  top: 0px;
+  left: 0px;
+  transform: scale(1) translate(-50%, -50%);
+  transform-origin: 0% 0%;
+`;
+
+const topRightStyle = css`
+  top: 0px;
+  right: 0px;
+  transform: scale(1) translate(50%, -50%);
+  transform-origin: 100% 0%;
+`;
+
+const bottomLeftStyle = css`
+  bottom: 0px;
+  left: 0px;
+  transform: scale(1) translate(-50%, 50%);
+  transform-origin: 0% 100%;
+`;
+
+const bottomRightStyle = css`
+  bottom: 0px;
+  right: 0px;
+  transform: scale(1) translate(50%, 50%);
+  transform-origin: 100% 100%;
+`;
+
+const placementStyleMap = {
+  'top-left': topLeftStyle,
+  'top-right': topRightStyle,
+  'bottom-left': bottomLeftStyle,
+  'bottom-right': bottomRightStyle,
+};
+
+const BadgeWrapper = styled.div`
+  display: inline-flex;
+  position: relative;
+`;
+
+const StandardBadge = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  place-content: center;
+  align-items: center;
+  position: absolute;
+  box-sizing: border-box;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  min-width: 20px;
+  padding: 0px 6px;
+  height: 20px;
+  border-radius: 10px;
+  z-index: 1;
+  transition: transform 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  background-color: ${(props) => props.$color};
+  color: #FFF;
+  ${(props) => placementStyleMap[props.$placement] || topRightStyle}
+`;
+
+const DotBadge = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 6px;
+  border-radius: 100%;
+  background-color: ${(props) => props.$color};
+  ${(props) => placementStyleMap[props.$placement] || topRightStyle}
+`;
+
+/**
+ * `Badge` 可以讓我們在其 children element 的右上角(預設位置)顯示一個小徽章，
+ * 通常用來表示需要處理的訊息數量，透過醒目的視覺形式來吸引用戶處理。
+*/
+const Badge = ({
+  children,
+  themeColor,
+  badgeContent,
+  placement,
+  max,
+  variant,
+  className,
+}) => {
+  const { makeColor } = useColor();
+  const color = makeColor({ themeColor });
+
+  return (
+    <BadgeWrapper>
+      {children}
+      {variant === 'dot' && (
+      <DotBadge
+        className={className}
+        $color={color}
+        $placement={placement}
+      />
+      )}
+      {variant === 'standard' && (
+      <StandardBadge
+        className={className}
+        $color={color}
+        $placement={placement}
+      >
+        {badgeContent > max ? `${max}+` : badgeContent}
+      </StandardBadge>
+      )}
+    </BadgeWrapper>
+  );
+};
+
+Badge.propTypes = {
+  /**
+   * 客製化樣式
+   */
+  className: PropTypes.string,
+  /**
+   * 展示內容
+   */
+  badgeContent: PropTypes.number,
+  /**
+   * 主題配色，primary、secondary 或是自己傳入色票
+   */
+  themeColor: PropTypes.oneOfType([PropTypes.oneOf(['primary', 'secondary']), PropTypes.string]),
+  /**
+   * 徽章位置
+   */
+  placement: PropTypes.oneOf(['top-left', 'top-right', 'bottom-left', 'bottom-right']),
+  /**
+   * 最大顯示值
+   */
+  max: PropTypes.number,
+  /**
+   * 變化模式
+   */
+  variant: PropTypes.oneOf(['standard', 'dot']),
+  /**
+   * 內容
+   */
+  children: PropTypes.element.isRequired,
+};
+
+Badge.defaultProps = {
+  className: '',
+  badgeContent: null,
+  themeColor: '#F85149',
+  placement: 'top-right',
+  variant: 'standard',
+  max: 99,
+};
 
 export default Badge;
--- src/stories/Badge.stories.jsx
+++ src/stories/Badge.stories.jsx
@@ -1,15 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+import MailIcon from '@material-ui/icons/Mail';
 import Badge from '../components/Badge';
+import TextField from '../components/TextField';
 
 export default {
   title: '數據展示元件/Badge',
   component: Badge,
 };
 
+const BadgePlacementWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  & > * {
+    margin: 20px;
+  }
+`;
+
+const SpaceBetween = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const VariantGroup = styled.div`
+  & > *:not(:first-child) {
+    margin-left: 20px;
+  }
+`;
+
 const Template = (args) => <Badge {...args} />;
+
+const TemplatePlacement = (args) => (
+  <BadgePlacementWrapper>
+    <Badge {...args} />
+    <Badge {...args} placement="top-left" />
+    <Badge {...args} placement="bottom-right" />
+    <Badge {...args} placement="bottom-left" />
+  </BadgePlacementWrapper>
+);
+
+const TemplateMax = (args) => {
+  const [badgeContent, setBadgeContent] = useState(120);
+
+  const handleOnChange = (event) => {
+    const { value } = event.target;
+    const num = Number(value);
+    if (Number.isInteger(num) && num > -1) {
+      setBadgeContent(Number(value));
+    }
+  };
+
+  return (
+    <SpaceBetween>
+      <Badge {...args} badgeContent={badgeContent} />
+      <TextField value={badgeContent} onChange={handleOnChange} />
+    </SpaceBetween>
+  );
+};
+
+const TemplateCustomColor = (args) => {
+  const [pickedColor, setPickedColor] = useState('#FE6B8B');
+
+  return (
+    <SpaceBetween>
+      <VariantGroup>
+        <Badge {...args} themeColor={pickedColor} />
+        <Badge {...args} themeColor={pickedColor} variant="dot" />
+      </VariantGroup>
+      <input
+        type="color"
+        value={pickedColor}
+        onChange={(event) => setPickedColor(event.target.value)}
+      />
+    </SpaceBetween>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
-  children: 'Badge',
+  children: <MailIcon />,
+  badgeContent: 7,
+};
+
+export const Placement = TemplatePlacement.bind({});
+Placement.args = {
+  children: <MailIcon />,
+  badgeContent: 7,
+};
+
+export const WithMax = TemplateMax.bind({});
+WithMax.args = {
+  max: 99,
+  children: <MailIcon />,
+};
+
+export const VariantDot = TemplatePlacement.bind({});
+VariantDot.args = {
+  children: <MailIcon />,
+  badgeContent: 7,
+  variant: 'dot',
+};
+
+export const CustomColor = TemplateCustomColor.bind({});
+CustomColor.args = {
+  children: <MailIcon />,
+  badgeContent: 7,
 };
