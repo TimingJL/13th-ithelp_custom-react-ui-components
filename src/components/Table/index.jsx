05c2ded991db80b09775dedf5b9200522041e4c5
taiming <taiming@bridgewell.com>
2021-09-29 10:41:52 +0800

    Add raw data table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,16 +1,66 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
+const StyledTable = styled.div`
+  display: inline-block;
+  border-collapse: collapse;
+  * {
+    border: 1px solid #000;
+  }
+`;
 
 /**
  * `Table` 顧名思義就是一個表格元件，用來整齊的顯示行列數據。
 */
-const Table = () => (
-  <div>
-    Table
-  </div>
+const Table = ({
+  columns, dataSource,
+}) => (
+  <StyledTable>
+    <thead>
+      <tr>
+        {
+          columns.map((column) => (
+            <th key={column.key}>
+              {column.title}
+            </th>
+          ))
+        }
+      </tr>
+    </thead>
+    <tbody>
+      {
+        dataSource.map((data) => (
+          <tr key={data.key}>
+            {
+              columns.map((column) => {
+                const { dataIndex } = column;
+                const foundCellData = column.render
+                  ? column.render(data[dataIndex])
+                  : data[dataIndex];
+                return (
+                  <td key={column.key}>
+                    {foundCellData}
+                  </td>
+                );
+              })
+            }
+          </tr>
+        ))
+      }
+    </tbody>
+  </StyledTable>
 );
 
-Table.propTypes = {};
-Table.defaultProps = {};
+Table.propTypes = {
+  columns: PropTypes.array,
+  dataSource: PropTypes.array,
+};
+
+Table.defaultProps = {
+  columns: [],
+  dataSource: [],
+};
 
 export default Table;
--- src/stories/Table.stories.jsx
+++ src/stories/Table.stories.jsx
@@ -7,7 +7,52 @@ export default {
   component: Table,
 };
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    // tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    // tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    // tags: ['cool', 'teacher'],
+  },
+];
+
 const Template = (args) => <Table {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  columns,
+  dataSource,
+};
