434697c1af69b4d2f4da7f2594037d6c91d9cf31
taiming <taiming@bridgewell.com>
2021-09-29 23:24:16 +0800

    Styling table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,64 +1,111 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
+const stickyLeftStyle = css`
+  position: sticky;
+  left: 0px;
+  z-index: 2;
+  &:after {
+    content: "";
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    width: 30px;
+    height: 100%;
+    box-shadow: inset 10px 0 8px -8px #00000026;
+    transform: translateX(100%);
+  }
+`;
+
 const StyledTable = styled.table`
-  display: inline-block;
   border-collapse: collapse;
   * {
     border: 1px solid #000;
+    box-sizing: border-box;
   }
+`;
+
+const Th = styled.th`
+  width: ${(props) => props.$width}px;
+  ${(props) => props.$fixed && stickyLeftStyle};
+`;
+
+const Td = styled.td`
+  background: #FFF;
+  ${(props) => props.$fixed && stickyLeftStyle};
 `;
 
 /**
  * `Table` 顧名思義就是一個表格元件，用來整齊的顯示行列數據。
 */
 const Table = ({
+  className,
   columns, dataSource,
 }) => (
-  <StyledTable>
-    <thead>
-      <tr>
+  <div style={{ width: '100%', overflow: 'auto' }}>
+    <StyledTable
+      className={className}
+      $columnsCount={columns.length}
+    >
+      <thead>
+        <tr>
+          {
+            columns.map((column) => (
+              <Th
+                key={column.key}
+                $width={column.width}
+                $fixed={column.fixed}
+              >
+                {column.title}
+              </Th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody>
         {
-          columns.map((column) => (
-            <th key={column.key}>
-              {column.title}
-            </th>
+          dataSource.map((data) => (
+            <tr key={data.key}>
+              {
+                columns.map((column) => {
+                  const { dataIndex } = column;
+                  const foundCellData = column.render
+                    ? column.render(data[dataIndex])
+                    : data[dataIndex];
+                  return (
+                    <Td key={column.key} $fixed={column.fixed}>
+                      {foundCellData}
+                    </Td>
+                  );
+                })
+              }
+            </tr>
           ))
         }
-      </tr>
-    </thead>
-    <tbody>
-      {
-        dataSource.map((data) => (
-          <tr key={data.key}>
-            {
-              columns.map((column) => {
-                const { dataIndex } = column;
-                const foundCellData = column.render
-                  ? column.render(data[dataIndex])
-                  : data[dataIndex];
-                return (
-                  <td key={column.key}>
-                    {foundCellData}
-                  </td>
-                );
-              })
-            }
-          </tr>
-        ))
-      }
-    </tbody>
-  </StyledTable>
+      </tbody>
+    </StyledTable>
+  </div>
 );
 
 Table.propTypes = {
+  /**
+   * 客製化樣式
+  */
+  className: PropTypes.string,
+  /**
+   * 描述表格欄位的配置
+  */
   columns: PropTypes.array,
+  /**
+   * 指定表格的數據內容
+  */
   dataSource: PropTypes.array,
 };
 
 Table.defaultProps = {
+  className: '',
   columns: [],
   dataSource: [],
 };
--- src/stories/Table.stories.jsx
+++ src/stories/Table.stories.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Table from '../components/Table';
+import Button from '../components/Button';
 
 export default {
   title: '數據展示元件/Table',
   component: Table,
 };
 
+const AntdStyle = styled(Table)`
+  width: 100%;
+  * {
+    border: none;
+    white-space: nowrap;
+    text-align: left;
+  }
+
+  th {
+    background: #fafafa;
+  }
+  td, th {
+    padding: 16px;
+  }
+  tr {
+    border-bottom: 1px solid #f0f0f0;
+  }
+  thead tr th {
+    position:sticky;
+    top:0;
+  }
+`;
+
 const columns = [
   {
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+    width: 130,
   },
   {
     title: 'Age',
     dataIndex: 'age',
     key: 'age',
+    width: 65,
   },
   {
     title: 'Address',
@@ -31,28 +58,69 @@ const dataSource = [
     name: 'John Brown',
     age: 32,
     address: 'New York No. 1 Lake Park',
-    // tags: ['nice', 'developer'],
   },
   {
     key: '2',
     name: 'Jim Green',
     age: 42,
     address: 'London No. 1 Lake Park',
-    // tags: ['loser'],
   },
   {
     key: '3',
     name: 'Joe Black',
     age: 32,
     address: 'Sidney No. 1 Lake Park',
-    // tags: ['cool', 'teacher'],
   },
 ];
 
 const Template = (args) => <Table {...args} />;
 
+const TemplateStyledTable = (args) => (
+  <AntdStyle {...args} />
+);
+
 export const Default = Template.bind({});
 Default.args = {
   columns,
+  dataSource,
+};
+
+export const AntdStyledTable = TemplateStyledTable.bind({});
+AntdStyledTable.args = {
+  columns,
+  dataSource,
+};
+
+export const StickyColumn = TemplateStyledTable.bind({});
+StickyColumn.args = {
+  columns: [
+    ...columns.map((column, index) => {
+      if (index === 0) {
+        return {
+          ...column,
+          fixed: true,
+        };
+      }
+      return column;
+    }),
+  ],
+  dataSource,
+};
+
+export const CustomContent = TemplateStyledTable.bind({});
+CustomContent.args = {
+  columns: [
+    ...columns,
+    {
+      title: '操作',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: () => (
+        <Button themeColor="secondary">
+          <span>刪除</span>
+        </Button>
+      ),
+    },
+  ],
   dataSource,
 };
