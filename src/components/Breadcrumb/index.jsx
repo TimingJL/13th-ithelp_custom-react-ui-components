4044d68b5db7420de8beeaa2cbd091f77a37cbfe
taiming <taiming@bridgewell.com>
2021-10-01 06:16:20 +0800

    Add description to Breadcrumb

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
+/**
+ * `Breadcrumb` 是一個導航元件，用於顯示當前系統層級結構中的路徑位置，並且點擊路徑能返回之前的頁面。
+ * 在系統有多個層級架構，並且希望能幫助用戶清楚知道自己目前層級位置，
+ * 及希望用戶能方便返回上面層級時，能夠使用麵包屑元件。
+*/
 const Breadcrumb = () => (
   <div>Breadcrumb</div>
 );
+
+Breadcrumb.propTypes = {};
+Breadcrumb.defaultProps = {};
 
 export default Breadcrumb;
