ff7e64c4d5521ddb46a2830cbf9633ea9d7877b3
taiming <taiming@bridgewell.com>
2021-10-02 20:19:39 +0800

    Init Select

---
+++ src/components/Select/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+/**
+ * `Select` 是一個下拉選擇器。觸發時能夠彈出一個菜單讓用戶選擇操作。
+*/
+const Select = () => (
+  <div>
+    Select
+  </div>
+);
+
+Select.propTypes = {};
+
+Select.defaultProps = {};
+
+export default Select;
---
+++ src/stories/Select.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Select from '../components/Select';
+
+export default {
+  title: '導航元件/Select',
+  component: Select,
+};
+
+const Template = (args) => <Select {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+
+};
