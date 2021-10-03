e1e724aa553b7eaf2b6a04362ad1aaa8f28d3bf0
taiming <taiming@bridgewell.com>
2021-10-04 01:17:04 +0800

    Add description

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const Drawer = () => (
+/**
+ * `Drawer` 抽屜元件，由螢幕邊緣滑出的浮動面版，
+ * 常見的應用是作為導航用途，例如 Navigation drawers。
+ */
+const Drawer = ({ children }) => (
   <div>
     Drawer
   </div>
 );
 
-Drawer.propTypes = {};
+Drawer.propTypes = {
+  /**
+   * 內容
+   */
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
+};
 
 Drawer.defaultProps = {};
 
