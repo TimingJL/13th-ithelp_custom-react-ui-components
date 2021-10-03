5f89fb9bc2a20736813c996898b454f4d143fd83
taiming <taiming@bridgewell.com>
2021-10-04 02:22:23 +0800

    Add Drawer

--- src/stories/Drawer.stories.jsx
+++ src/stories/Drawer.stories.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from '../components/Button';
 import Drawer from '../components/Drawer';
 
 export default {
@@ -7,9 +8,21 @@ export default {
   component: Drawer,
 };
 
-const Template = (args) => <Drawer {...args} />;
+const Template = (args) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Drawer</Button>
+      <Drawer
+        {...args}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <div style={{ width: 300 }}>Drawer content</div>
+      </Drawer>
+    </>
+  );
+};
 
 export const Default = Template.bind({});
-Default.args = {
-  children: 'Drawer',
-};
