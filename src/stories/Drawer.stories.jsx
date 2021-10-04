2e37b68196c3b4a1a9a8bc3099b4595f6621d9e2
taiming <taiming@bridgewell.com>
2021-10-04 13:16:17 +0800

    Add placement case

--- src/stories/Drawer.stories.jsx
+++ src/stories/Drawer.stories.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../components/Button';
 import Drawer from '../components/Drawer';
@@ -8,12 +9,18 @@ export default {
   component: Drawer,
 };
 
+const ButtonGroup = styled.div`
+  & > *:not(:first-child) {
+    margin-left: 20px;
+  }
+`;
+
 const Template = (args) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <Button onClick={() => setIsOpen(true)}>Open Drawer</Button>
+      <Button variant="outlined" onClick={() => setIsOpen(true)}>Open Drawer</Button>
       <Drawer
         {...args}
         isOpen={isOpen}
@@ -25,4 +32,46 @@ const Template = (args) => {
   );
 };
 
+const TemplatePlacement = (args) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [placement, setPlacement] = useState('');
+  const [drawerStyle, setDrawerStyle] = useState({});
+
+  const handleOpenDrawer = ({ clickedPlacement }) => {
+    setIsOpen(true);
+    setPlacement(clickedPlacement);
+    if (['left', 'right'].indexOf(clickedPlacement) > -1) {
+      setDrawerStyle({
+        width: 300,
+      });
+    }
+    if (['top', 'bottom'].indexOf(clickedPlacement) > -1) {
+      setDrawerStyle({
+        height: 300,
+      });
+    }
+  };
+
+  return (
+    <>
+      <ButtonGroup>
+        <Button variant="outlined" onClick={() => handleOpenDrawer({ clickedPlacement: 'top' })}>Top</Button>
+        <Button variant="outlined" onClick={() => handleOpenDrawer({ clickedPlacement: 'right' })}>Right</Button>
+        <Button variant="outlined" onClick={() => handleOpenDrawer({ clickedPlacement: 'bottom' })}>Bottom</Button>
+        <Button variant="outlined" onClick={() => handleOpenDrawer({ clickedPlacement: 'left' })}>Left</Button>
+      </ButtonGroup>
+      <Drawer
+        {...args}
+        isOpen={isOpen}
+        placement={placement}
+        onClose={() => setIsOpen(false)}
+      >
+        <div style={drawerStyle}>{`${placement} drawer`}</div>
+      </Drawer>
+    </>
+  );
+};
+
 export const Default = Template.bind({});
+
+export const Placement = TemplatePlacement.bind({});
