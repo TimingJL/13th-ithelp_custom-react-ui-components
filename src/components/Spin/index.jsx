566f047e2f51fb5a31a5d45455e251956592ed1f
taiming <taiming@bridgewell.com>
2021-10-06 18:48:23 +0800

    Init Spin

---
+++ src/components/Spin/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spin = () => (
+  <div>
+    Spin
+  </div>
+);
+
+Spin.propTypes = {};
+
+Spin.defaultProps = {};
+
+export default Spin;
---
+++ src/stories/Spin.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Spin from '../components/Spin';
+
+export default {
+  title: '反饋元件/Spin',
+  component: Spin,
+};
+
+const Template = (args) => <Spin {...args} />;
+
+export const Default = Template.bind({});
