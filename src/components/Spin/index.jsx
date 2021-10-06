412b9d55885449dec726d594647907f3e07ad984
taiming <taiming@bridgewell.com>
2021-10-07 00:27:01 +0800

    Add Spin

--- src/components/Spin/index.jsx
+++ src/components/Spin/index.jsx
@@ -1,17 +1,112 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const StyledSpin = styled.div`
+  display: inline-flex;
+  svg {
+    color: ${(props) => props.theme.color.primary};
+  }
+`;
+
+const SpinContainer = styled.div`
+  display: inline-flex;
+  svg {
+    color: ${(props) => props.theme.color.primary};
+  }
+  position: relative;
+`;
+
+const Indicator = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 4;
+  display: block;
+  width: 100%;
+  height: 100%;
+  max-height: 400px;
+  & > * {
+    position: absolute;
+    top: calc(50% - ${(props) => props.$indicatorSize.height / 2}px);
+    left: calc(50% - ${(props) => props.$indicatorSize.width / 2}px);
+  }
+`;
+
+const Mask = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #FFF;
+  opacity: 0.8;
+`;
 
 /**
  * `Spin` 是一個載入狀態元件，當頁面正在處理非同步行為，
  * 或需要讓用戶等待的作業時，用來顯示以緩解用戶等待的焦慮。
 */
-const Spin = () => (
-  <div>
-    Spin
-  </div>
-);
+const Spin = ({ indicator, isLoading, children }) => {
+  const indicatorRef = useRef();
+  const [indicatorSize, setIndicatorSize] = useState({
+    width: 0,
+    height: 0,
+  });
 
-Spin.propTypes = {};
+  useEffect(() => {
+    const indicatorElem = indicatorRef.current?.children[0];
+    setIndicatorSize({
+      width: indicatorElem?.clientWidth,
+      height: indicatorElem?.clientHeight,
+    });
+  }, [indicatorRef, isLoading]);
 
-Spin.defaultProps = {};
+  if (!children) {
+    return (
+      <StyledSpin>
+        {indicator}
+      </StyledSpin>
+    );
+  }
+  return (
+    <SpinContainer>
+      {children}
+      {isLoading && (
+        <>
+          <Mask />
+          <Indicator
+            ref={indicatorRef}
+            className="spin__indicator"
+            $indicatorSize={indicatorSize}
+          >
+            {indicator}
+          </Indicator>
+        </>
+      )}
+    </SpinContainer>
+  );
+};
+
+Spin.propTypes = {
+  /**
+   * 自定義載入符號
+  */
+  indicator: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  /**
+   * 是否載入中
+  */
+  isLoading: PropTypes.bool,
+  /**
+   * 內容
+  */
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+};
+
+Spin.defaultProps = {
+  indicator: <CircularProgress />,
+  isLoading: false,
+  children: '',
+};
 
 export default Spin;
--- src/stories/Spin.stories.jsx
+++ src/stories/Spin.stories.jsx
@@ -1,12 +1,125 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import Spin from '../components/Spin';
+import Switch from '../components/Switch';
+import FormControl from '../components/FormControl';
+import { FaSpinner } from '../components/Icons/FaSpinner';
 
 export default {
   title: '反饋元件/Spin',
   component: Spin,
 };
 
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const RotateContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  animation: ${rotateAnimation} 1000ms ease-in-out infinite;
+`;
+
+const MockContent = styled.div`
+  max-width: 500px;
+  padding: 20px;
+  max-height: 500px;
+  overflow-y: auto;
+  border: 1px solid #B9B9B9;
+`;
+
 const Template = (args) => <Spin {...args} />;
 
+const TemplateSpinContainer = (args) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [useCustomIndicator, setUseCustomIndicator] = useState(false);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Spin
+        isLoading={isChecked}
+        indicator={useCustomIndicator ? (
+          <RotateContainer>
+            <FaSpinner />
+          </RotateContainer>
+        ) : undefined}
+      >
+        <MockContent>
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using Lorem Ipsum is
+            that it has a more-or-less normal distribution of letters, as opposed to using
+            Content here, content here, making it look like readable English. Many desktop
+            publishing packages and web page editors now use Lorem Ipsum as their default model text,
+            and a search for lorem ipsum will uncover many web sites still in their infancy.
+            Various versions have evolved over the years,
+            sometimes by accident, sometimes on purpose (injected humour and the like).
+          </p>
+          <p>
+            Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using Lorem Ipsum is
+            that it has a more-or-less normal distribution of letters, as opposed to using
+            Content here, content here, making it look like readable English. Many desktop
+            publishing packages and web page editors now use Lorem Ipsum as their default model text,
+            and a search for lorem ipsum will uncover many web sites still in their infancy.
+            Various versions have evolved over the years,
+            sometimes by accident, sometimes on purpose (injected humour and the like).
+          </p>
+          <p>
+            Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using Lorem Ipsum is
+            that it has a more-or-less normal distribution of letters, as opposed to using
+            Content here, content here, making it look like readable English. Many desktop
+            publishing packages and web page editors now use Lorem Ipsum as their default model text,
+            and a search for lorem ipsum will uncover many web sites still in their infancy.
+            Various versions have evolved over the years,
+            sometimes by accident, sometimes on purpose (injected humour and the like).
+          </p>
+        </MockContent>
+      </Spin>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <FormControl
+          placement="left"
+          label="是否正在載入中"
+        >
+          <Switch
+            onChange={setIsChecked}
+          />
+        </FormControl>
+        <FormControl
+          placement="left"
+          label="自定義載入符號"
+          style={{ marginTop: 20 }}
+        >
+          <Switch
+            onChange={setUseCustomIndicator}
+          />
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
 export const Default = Template.bind({});
+
+export const CustomIndicator = Template.bind({});
+CustomIndicator.args = {
+  indicator: (
+    <RotateContainer>
+      <FaSpinner />
+    </RotateContainer>
+  ),
+};
+
+export const SpinAsContainer = TemplateSpinContainer.bind({});
