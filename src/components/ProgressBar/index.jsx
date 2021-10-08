4f5d58ba9ab2013978a1a3bb435c96950fe09b03
taiming <taiming@bridgewell.com>
2021-10-08 23:49:40 +0800

    Add progress description

---
+++ src/components/ProgressBar/index.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+/**
+ * `Progress bar` 是能夠展示當前進度的進度條元件。
+ * 當一個操作需要顯示目前百分比，或是需要較長時間等待運行的時候，
+ * 能夠使用這樣的元件提示用戶目前進度，藉此來緩解用戶等待的焦慮感，
+ * 或者提供使用者完成複雜任務的成就感。
+*/
+const ProgressBar = () => (
+  <div>
+    progressBar
+  </div>
+);
+
+ProgressBar.propTypes = {};
+
+ProgressBar.defaultProps = {};
+
+export default ProgressBar;
---
+++ src/stories/ProgressBar.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ProgressBar from '../components/ProgressBar';
+
+export default {
+  title: '反饋元件/ProgressBar',
+  component: ProgressBar,
+};
+
+const Template = (args) => <ProgressBar {...args} />;
+
+export const Default = Template.bind({});
