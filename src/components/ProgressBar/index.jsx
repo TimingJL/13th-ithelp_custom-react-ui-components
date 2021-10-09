ab54ce7b88f175728fd56cc58533d214e57d9405
taiming <taiming@bridgewell.com>
2021-10-09 22:02:59 +0800

    Add ProgressBar

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,6 +1,70 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-// import PropTypes from 'prop-types';
+import styled, { css, keyframes } from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { useColor } from 'hooks/useColor';
+
+const slide = keyframes`
+  from {
+    left: -150%;
+  }
+  to {
+    left: 100%;
+  }
+`;
+
+const activeAnimation = css`
+  position: relative;
+  overflow: hidden;
+  &:before {
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 80px;
+    top: 0px;
+    background: linear-gradient(to right, transparent 0%, #FFFFFF99 50%, transparent 100%);
+    animation: ${slide} 1s cubic-bezier(0.4, 0.0, 0.2, 1) infinite;
+    box-shadow: 0 4px 10px 0 #FFFFFF33;
+  }
+`;
+
+const StyledProgressBar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Trail = styled.div`
+  width: 100%;
+  height: 8px;
+  background: #EEE;
+  border-radius: 50px;
+`;
+
+const Track = styled.div`
+  background: ${(props) => props.$color};
+  width: ${(props) => props.$value}%;
+  height: 8px;
+  border-radius: 50px;
+  transition: width 0.2s;
+  ${(props) => props.$isStatusActive && activeAnimation}
+`;
+
+const Info = styled.div`
+  flex: 0 0 50px;
+  text-align: right;
+  margin-left: 4px;
+`;
+
+const formatValue = (value) => {
+  if (value > 100) {
+    return 100;
+  }
+  if (value < 0) {
+    return 0;
+  }
+  return value;
+};
 
 /**
  * `Progress bar` 是能夠展示當前進度的進度條元件。
@@ -8,14 +72,64 @@ import React from 'react';
  * 能夠使用這樣的元件提示用戶目前進度，藉此來緩解用戶等待的焦慮感，
  * 或者提供使用者完成複雜任務的成就感。
 */
-const ProgressBar = () => (
-  <div>
-    progressBar
-  </div>
-);
+const ProgressBar = ({
+  className,
+  value,
+  themeColor,
+  showInfo,
+  isStatusActive,
+}) => {
+  const { makeColor } = useColor();
+  const color = makeColor({ themeColor });
 
-ProgressBar.propTypes = {};
+  return (
+    <StyledProgressBar className={className}>
+      <Trail className="progress-bar__trail">
+        <Track
+          className="progress-bar__track"
+          $color={color}
+          $value={formatValue(value)}
+          $isStatusActive={isStatusActive}
+        />
+      </Trail>
+      {showInfo && (
+        <Info className="progress-bar__info">
+          {`${value}%`}
+        </Info>
+      )}
+    </StyledProgressBar>
+  );
+};
 
-ProgressBar.defaultProps = {};
+ProgressBar.propTypes = {
+  /**
+   * 客製化樣式
+   */
+  className: PropTypes.string,
+  /**
+   * 進度
+   */
+  value: PropTypes.number,
+  /**
+   * 主題配色，primary、secondary 或是自己傳入色票
+   */
+  themeColor: PropTypes.oneOfType([PropTypes.oneOf(['primary', 'secondary']), PropTypes.string]),
+  /**
+   * 是否顯示進度數值
+   */
+  showInfo: PropTypes.bool,
+  /**
+   * 是否顯示等待進度動畫
+   */
+  isStatusActive: PropTypes.bool,
+};
+
+ProgressBar.defaultProps = {
+  className: '',
+  value: 0,
+  themeColor: 'primary',
+  showInfo: true,
+  isStatusActive: false,
+};
 
 export default ProgressBar;
--- src/stories/ProgressBar.stories.jsx
+++ src/stories/ProgressBar.stories.jsx
@@ -1,12 +1,142 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import ProgressBar from '../components/ProgressBar';
+import Button from '../components/Button';
 
 export default {
   title: '反饋元件/ProgressBar',
   component: ProgressBar,
 };
 
-const Template = (args) => <ProgressBar {...args} />;
+const Container = styled.div`
+  max-width: 500px;
+  width: 100%;
+  & > *:not(:first-child) {
+    margin-top: 20px;
+  }
+`;
+
+const GradientProgressBar = styled(ProgressBar)`
+  .progress-bar__track {
+    background: linear-gradient(45deg, #FF8E53 30%, #FE6B8B 90%);
+  }
+`;
+
+const Template = (args) => (
+  <Container>
+    <ProgressBar {...args} />
+    <ProgressBar {...args} value={50} />
+    <ProgressBar {...args} value={75} />
+    <ProgressBar {...args} value={100} />
+    {args.showInfo && <ProgressBar {...args} value={120} />}
+  </Container>
+);
+
+const TemplateGradient = (args) => (
+  <Container>
+    <GradientProgressBar {...args} />
+    <GradientProgressBar {...args} value={50} />
+    <GradientProgressBar {...args} value={75} />
+    <GradientProgressBar {...args} value={100} />
+    {args.showInfo && <GradientProgressBar {...args} value={120} />}
+  </Container>
+);
+
+const TemplateCustomColor = (args) => {
+  const defaultColor = '#FE6B8B';
+  const [pickedColor, setPickedColor] = useState(defaultColor);
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 20 }}>
+        <input
+          type="color"
+          value={pickedColor}
+          onChange={(event) => setPickedColor(event.target.value)}
+        />
+      </div>
+      <Container>
+        <ProgressBar {...args} themeColor={pickedColor} />
+        <ProgressBar {...args} value={50} themeColor={pickedColor} />
+        <ProgressBar {...args} value={75} themeColor={pickedColor} />
+        <ProgressBar {...args} value={100} themeColor={pickedColor} />
+        {args.showInfo && <ProgressBar {...args} value={120} themeColor={pickedColor} />}
+      </Container>
+    </div>
+  );
+};
+
+const TemplateTransition = (args) => {
+  const [playKey, setPlayKey] = useState(true);
+  const [transitionValue, setTransitionValue] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+    setTransitionValue(0);
+    if (playKey) {
+      setPlayKey(false);
+      intervalId = setInterval(() => {
+        setTransitionValue((prev) => {
+          if (prev >= 120) {
+            clearInterval(intervalId);
+          }
+          return prev + 1;
+        });
+      }, 30);
+    }
+  }, [playKey]);
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 20 }}>
+        <Button onClick={() => setPlayKey(true)}>
+          重播
+        </Button>
+      </div>
+      <Container>
+        <ProgressBar {...args} />
+        <ProgressBar {...args} value={transitionValue < 50 ? transitionValue : 50} />
+        <ProgressBar {...args} value={transitionValue < 75 ? transitionValue : 75} />
+        <ProgressBar {...args} value={transitionValue < 100 ? transitionValue : 100} />
+        {args.showInfo && (
+          <ProgressBar {...args} value={transitionValue < 120 ? transitionValue : 120} />
+        )}
+      </Container>
+    </div>
+  );
+};
 
 export const Default = Template.bind({});
+Default.args = {
+  showInfo: true,
+};
+
+export const WithoutInfo = Template.bind({});
+WithoutInfo.args = {
+  showInfo: false,
+};
+
+export const CustomColor = TemplateCustomColor.bind({});
+CustomColor.args = {
+  showInfo: true,
+  themeColor: '',
+};
+
+export const GradientTrack = TemplateGradient.bind({});
+GradientTrack.args = {
+  showInfo: true,
+};
+
+export const HasActiveAnimation = Template.bind({});
+HasActiveAnimation.args = {
+  showInfo: true,
+  isStatusActive: true,
+};
+
+export const HasTransition = TemplateTransition.bind({});
+HasTransition.args = {
+  showInfo: true,
+  isStatusActive: true,
+};
