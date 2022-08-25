58d7b0b4880725928cb9b4cb364eedb905ee1311
taiming <eefozeo@gmail.com>
2022-08-25 09:32:54 +0800

    Update css

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
 import React from 'react';
-// import { message, Button } from 'antd';
-// import Button from 'mui'
-// import Button from '@material-ui/core/Button';
-// import { Button } from 'antd';
-import styled from 'styled-components';
-// import SwitchSandbox from './sandbox/SwitchSandbox';
+import logo from './logo.svg';
+import './App.css';
 import { Button } from './lib';
 
-const Container = styled.div`
-  display: flex;
-  margin: 50px;
-  & > * {
-    margin-left: 20px;
-  }
-`;
-
-const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
-
-const App = () => {
-  const info = () => {
-    // message.info('This is a normal message');
-  };
-
+function App() {
   return (
-    <Container>
-      <Button>Hello?</Button>
-      {/* <Button type="primary" onClick={info}>
-        Display normal message
-      </Button> */}
-      {/* <Button type="primary" onClick={info}></Button> */}
-      {/* <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Button variant="dashed">Dashed</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="link">Link</Button> */}
-
-      {/* <Button type="primary">
-        Contained
-      </Button>
-      <Button>Outlined</Button>
-      <Button type="dashed">
-        Dashed
-      </Button>
-      <Button type="text">
-        Text
-      </Button>
-      <Button type="link">
-        Link
-      </Button> */}
-      {/* <SwitchSandbox /> */}
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button>Learn More</Button>
+      </header>
+    </div>
   );
-};
+}
+
 export default App;
