ae85169885e4e3d06761185f025c4f751b53b2ad
taiming <eefozeo@gmail.com>
2022-08-25 09:53:56 +0800

    Clear

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-// import RadioSandbox from './sandbox/RadioSandbox';
-// import InputSandbox from './sandbox/InputSandbox';
-// import SwitchSandbox from './sandbox/SwitchSandbox';
-// import SliderSandbox from './sandbox/SliderSandbox';
-// import UploadSandbox from './sandbox/UploadSandbox';
-// import CarouselSandbox from './sandbox/CarouselSandbox';
-// import TabsSandbox from './sandbox/TabsSandbox';
-import Switch from './components/Switch';
-// import Carousel3DCSSSandbox from './sandbox/Carousel3DCSSSandbox';
 
 const App = () => (
   <div style={{ padding: 60 }}>
-    {/* <RadioSandbox /> */}
-    {/* <InputSandbox /> */}
-    {/* <SwitchSandbox /> */}
-    {/* <TabsSandbox /> */}
-    <Switch isChecked />
-    {/* <Carousel3DCSSSandbox /> */}
+    <h1>13th-ithelp_custom-react-ui-components</h1>
   </div>
 );
 export default App;
