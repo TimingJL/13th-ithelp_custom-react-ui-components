721f633b427bf998873aa9b1a0e490a45316642e
taiming <eefozeo@gmail.com>
2022-09-23 22:17:29 +0800

    Update useColor

--- src/lib/hooks/useColor.jsx
+++ src/lib/hooks/useColor.jsx
@@ -1,19 +1,10 @@
-// import { useTheme } from 'styled-components';
 import { theme as defaultTheme } from '../theme';
 
 export const useColor = () => {
   const theme = defaultTheme.default;
 
   const makeColor = ({ themeColor, isDisabled }) => {
-    /**
-     * Color codes regular expression
-     * https://regexr.com/39cgj
-     */
-    const colorRegex = new RegExp(/(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\([^)]*\)/);
-    const isValidColorCode = colorRegex.test(themeColor?.toLocaleLowerCase());
-    const madeColor = isValidColorCode
-      ? themeColor
-      : (theme.color[themeColor] || theme.color.primary);
+    const madeColor = theme.color[themeColor] || themeColor;
     return isDisabled
       ? theme.color.disable
       : madeColor;
